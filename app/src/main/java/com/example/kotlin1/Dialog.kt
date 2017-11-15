5bf782361608ef07799077255b90d2856b744b43
Denis Evdokumov <denis.vd@rambler.ru>
2017-11-15 05:03:54 +0200

    added a new footer [version2]

---
+++ app/src/main/java/com/example/kotlin1/Dialog.kt
@@ -0,0 +1,22 @@
+package com.example.kotlin1
+
+import android.app.AlertDialog
+import android.app.Dialog
+import android.app.DialogFragment
+import android.content.DialogInterface
+import android.os.Bundle
+
+/**
+ * Created by Денис on 10.11.2017.
+// */
+//public class Dialog : DialogFragment(){
+//    //val v = vinner
+//    override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
+//        super.onCreateDialog(savedInstanceState)
+//
+////        val alerDialogBilder = AlertDialog.Builder(activity)
+////               .setTitle(R.string.viner)
+////                .setPositiveButton(R.string.yes,DialogInterface.OnClickListener{activity.onCreate()})
+////
+////    }
+//}
--- app/src/main/java/com/example/kotlin1/MainActivity.kt
+++ app/src/main/java/com/example/kotlin1/MainActivity.kt
@@ -5,116 +5,44 @@ import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.Button
+import android.widget.TableLayout
+import android.widget.TableRow
 import android.widget.Toast
+import com.example.kotlin1.R.attr.colorButtonNormal
 import kotlinx.android.synthetic.main.activity_main.*
 
 
 class MainActivity : AppCompatActivity() {
 
+    val rowN = 20
+    val butN = 20
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-
+        devbuttins()
     }
 
-    protected fun onClic (view :View){
-        val buttonclick = view as  Button
-        var idB = 0
-        when (buttonclick.id){
-            R.id.button1 -> idB =1
-            R.id.button2 -> idB =2
-            R.id.button3 -> idB =3
-            R.id.button4 -> idB =4
-            R.id.button5 -> idB =5
-            R.id.button6 -> idB =6
-            R.id.button7 -> idB =7
-            R.id.button8 -> idB =8
-            R.id.button9 -> idB =9
+    fun devbuttins(){
+        for (i in 0..rowN){
+            val tableRaw = TableRow (this)
+            table.addView(tableRaw)
+            for (j in 0..butN){
+                val button = Button (this)
+                tableRaw.addView(button)
+                button.text = "$i $j"
+                if (i==rowN-5 && j==butN-5){
+                    button.setFocusableInTouchMode(true)
+                    button.requestFocus()
+
+
+                }
+
+            }
         }
-        //Toast.makeText(this,idB.toString(),Toast.LENGTH_LONG).show()
-        Play(idB,buttonclick)
-    }
-    var player = 1
-    var player1 = ArrayList<Int>()
-    var player2 = ArrayList<Int>()
-    var winer = 0
-
-
-    fun Play (idB: Int,buttonclick: Button){
-
-        if (player ==1 ){
-            player1.add(idB)
-            buttonclick.text="X"
-            buttonclick.setBackgroundColor(Color.GREEN)
-            player=2
-        }else{
-            player2.add(idB)
-            buttonclick.text="O"
-            buttonclick.setBackgroundColor(Color.BLUE)
-            player=1
-
-        }
-        CheckWin()
     }
 
-    fun CheckWin(){
-
-        if(player1.contains(1)&&player1.contains(2)&&player1.contains(3)){
-            winer=1
-        }
-        if(player2.contains(1)&&player2.contains(2)&&player2.contains(3)){
-            winer=2
-        }
-
-        if(player1.contains(4)&&player1.contains(5)&&player1.contains(6)){
-            winer=1
-        }
-        if(player2.contains(4)&&player2.contains(5)&&player2.contains(6)){
-            winer=2
-        }
-
-        if(player1.contains(7)&&player1.contains(8)&&player1.contains(9)){
-            winer=1
-        }
-        if(player2.contains(7)&&player2.contains(8)&&player2.contains(9)){
-            winer=2
-        }
-
-        if(player1.contains(1)&&player1.contains(5)&&player1.contains(9)){
-            winer=1
-        }
-        if(player2.contains(1)&&player2.contains(5)&&player2.contains(9)){
-            winer=2
-        }
-        if(player1.contains(3)&&player1.contains(5)&&player1.contains(7)){
-            winer=1
-        }
-        if(player2.contains(3)&&player2.contains(5)&&player2.contains(7)){
-            winer=2
-        }
-        if(player1.contains(3)&&player1.contains(6)&&player1.contains(9)){
-            winer=1
-        }
-        if(player2.contains(3)&&player2.contains(6)&&player2.contains(9)){
-            winer=2
-        }
-        if(player1.contains(2)&&player1.contains(5)&&player1.contains(8)){
-            winer=1
-        }
-        if(player2.contains(2)&&player2.contains(5)&&player2.contains(8)){
-            winer=2
-        }
-        if(player1.contains(1)&&player1.contains(4)&&player1.contains(7)){
-            winer=1
-        }
-        if(player2.contains(1)&&player2.contains(4)&&player2.contains(7)){
-            winer=2
-        }
-        if (winer != 0){
-            Toast.makeText(this,"winner $winer",Toast.LENGTH_LONG).show()
-
-        }
-    }
 
 }
+
+
